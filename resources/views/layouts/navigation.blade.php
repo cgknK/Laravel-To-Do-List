e4339ff51fad0aade3437c7d93cb1467cfedf04a
Your Name <you@example.com>
2024-02-12 02:22:27 +0300

    dashbord'dan çıkmıyor fakat çıksa daha iyi xd

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <!--x-nav-link :href="route('note-s.index')" :active="request()->routeIs('note-s.index')" method="GET"-->
-                    <x-nav-link :href="route('note-s.index')" :active="request()->routeIs('note-s')">
+                    <x-nav-link :href="route('note-s.index')" :active="request()->routeIs('note-s.index')">
                         {{ __('Notes/ToDo List') }}
                     </x-nav-link>
                 </div>
--- resources/views/mylayout.blade.php
+++ resources/views/mylayout.blade.php
@@ -7,7 +7,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-@yield('content')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</x-app-layout>
+
+
 </body>
 </html>
